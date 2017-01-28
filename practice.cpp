ae2e10c18252ee09f36f2edffa0d83179514b9c6
dxy159 <richard.ni.wchs@gmail.com>
2017-01-27 19:05:35 -0500

    assignment 1 half complete

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int x, y;
+	std::cout << "Enter a number: " << std::endl;
+	std::cin >> x;
+	std::cout << "Enter a second number: " << std::endl;
+	std::cin >> y;
+	std::cout << x << std::endl;
+	return 0;
+}
