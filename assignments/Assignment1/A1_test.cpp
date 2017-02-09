b2071434c3373703802f0ab2ea800cb2077f3cb5
dxy159 <richard.ni.wchs@gmail.com>
2017-02-09 16:35:35 -0500

    finished assignment 1

--- assignments/Assignment1/A1_test.cpp
+++ assignments/Assignment1/A1_test.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 
 // Question 1
-void test1(int a, int* b);
-// int main() {
-//      int x = 3;
-//      test1(x, &x);
-// }
-void test1(int a, int* b) {
- if (&a == b) {
-   std:: cout << "equal!";
- }
- else {
-   std:: cout << "not equal";
- } 
-}
-
-// Question 2
-int* func1();
-int* func2();
-// int main() {
-//  int* a = func1();
-//  int* b = func2();
-//  if (a == b) {
-//    std:: cout << "equal";
+// void test1(int a, int* b);
+// // int main() {
+// //      int x = 3;
+// //      std::cout << &x;
+// //      test1(x, &x);
+// // }
+// void test1(int a, int* b) {
+//   std::cout << &a << b;
+//  if (&a == b) {
+//    std:: cout << "equal!";
 //  }
 //  else {
-//    std :: cout << "not equal";
-//  }
+//    std:: cout << "not equal";
+//  } 
 // }
-int* func1() {
-  int x;
-  return &x; 
-}
-int * func2() {
-  int x;
-  return &x; 
-}
+
+// Question 2
+   int* func1();
+   int* func2();
+   int main() {
+     int* a = func1();
+     int* b = func2();
+     if (a == b) {
+       std:: cout << "equal";
+     }
+     else {
+       std :: cout << "not equal";
+} }
+   int* func1() {
+     int x;
+     std::cout << &x << std::endl;
+return &x; }
+   int * func2() {
+     int x;
+     std::cout << &x << std::endl;
+return &x; }
+
 
 // Question 3
 // int main() {
@@ -54,13 +56,13 @@ int * func2() {
 // }
 
 // Question 5
-int main() {
-	char name[10];
-	int x = 0;
-	std::cout << "Enter your name" << std::endl;
-	std::cin >> name;
-	std::cout << x << " ";
-}
+// int main() {
+// 	char name[10];
+// 	int x = 0;
+// 	std::cout << "Enter your name" << std::endl;
+// 	std::cin >> name;
+// 	std::cout << x << " ";
+// }
 
 
 
